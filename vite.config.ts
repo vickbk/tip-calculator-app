fd87e84939754538f51fe9c3c0a0f319503be94c
vickbk <vickbake8@gmail.com>
2026-01-21 11:08:14 +0200

    initial test config with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
+/// <reference types="vitest/config" />
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
