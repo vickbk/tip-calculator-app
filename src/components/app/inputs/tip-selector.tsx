9431d0601a6be53a4e8f59cbc59a44bd412ead3c
vickbk <vickbake8@gmail.com>
2026-01-23 12:34:18 +0200

    unchecking tips on custom select

--- src/components/app/inputs/tip-selector.tsx
+++ src/components/app/inputs/tip-selector.tsx
@@ -1,18 +1,35 @@
 import { SROnly } from "@/components/shared/SROnly";
+import { useState } from "react";
 
+const TIPS = [5, 10, 15, 25, 50];
 export const TipSelector = () => {
+  const [checked, setChecked] = useState<number | null>(null);
   return (
     <fieldset>
       <legend>Select Tip %</legend>
-      {[5, 10, 15, 25, 50].map((tip) => (
+      {TIPS.map((tip) => (
         <label key={tip}>
           <SROnly>tip:</SROnly> {tip}%{" "}
-          <input type="radio" name="tip" value={tip} className="sr-only" />
+          <input
+            type="radio"
+            name="tip"
+            value={tip}
+            className="sr-onlys"
+            checked={checked === tip}
+            onChange={({ target }) => {
+              if (target.checked) setChecked(tip);
+            }}
+          />
         </label>
       ))}
       <label>
         <SROnly>Custom type</SROnly>{" "}
-        <input type="number" name="tip" placeholder="Custom" />
+        <input
+          type="number"
+          name="tip"
+          placeholder="Custom"
+          onFocus={() => setChecked(null)}
+        />
       </label>
     </fieldset>
   );
