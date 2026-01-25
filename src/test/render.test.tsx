879877e20f9c26bb2756b99a984667bc98a10de7
vickbk <vickbake8@gmail.com>
2026-01-25 12:23:25 +0200

    modernizing tests code

--- src/test/render.test.tsx
+++ src/test/render.test.tsx
@@ -1,17 +1,15 @@
-import { SplitCalculator } from "@/components/app/split-calculator";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "../App";
 
 describe("App", () => {
-  it("renders", () => {
+  it("renders", async () => {
     render(<App />);
     expect(screen.queryByText("Tip Calculator App")).toBeVisible();
-  });
-});
-
-describe("Tip Calculator", () => {
-  it("renders", () => {
-    render(<SplitCalculator />);
-    expect(screen.queryByText("Calculate tips on your bills")).toBeVisible();
+    await userEvent.type(screen.getByLabelText(/Bill/), "100");
+    await userEvent.click(screen.getByLabelText(/10%/));
+    await userEvent.type(screen.getByLabelText(/People/), "3");
+    expect(screen.getByText("$3.33")).toBeVisible(); // Tip Amount Per Person
+    expect(screen.getByText("$36.67")).toBeVisible(); // Total Amount Per Person
   });
 });
